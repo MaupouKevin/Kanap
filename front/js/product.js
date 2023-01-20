e353baddf3399ddc3bd30f98d89f4fdc8fd85f29
Kévin Maupou <maupou.kevin@gmail.com>
2023-01-20 20:51:37 +0100

    fonction localstorage

--- front/js/product.js
+++ front/js/product.js
@@ -57,4 +57,56 @@ let cartButton = document.querySelector("#addToCart");
     console.log("Il y a eu un problème avec l'opération fetch: " + error.message);
   });
   
+// PARTIE QUI TRAITE ET ENREGISTRE LES DONNÉES DANS LE localStorage
+//--------------------------------------------------------------------------------
 
+// Fonction qui sauvegarde les données du panier (LocalStorage).
+const saveBasket = (cartContent) => {
+  localStorage.setItem("basketItems", JSON.stringify(cartContent));
+};
+
+// Fonction qui récupère les données du panier(LocalStorage).
+const getBasket = () => {
+  let basket = localStorage.getItem("basketItems");
+
+  if (basket == null) {
+    return [];
+  } else {
+    return JSON.parse(basket);
+  }
+};
+
+let basket = getBasket();
+
+// Fonction qui va ajouter des produits dans le panier (localStorage).
+const addBasket = (product) => {
+ 
+  // Vérifie si un canapé stocké dans le LocalStorage (variable basket) a déjà le même ID et la même couleur, si c'est pas le cas alors retourne : "undefined".
+  let findProduct = basket.find((p) => p.id == product.id && p.color == product.color);
+
+  // Si le produit existe déjà dans le localStorage avec le même id et la même couleur alors on incrémente la quatité.
+  if (findProduct != undefined) {
+
+    if (findProduct.quantity >= 100) {
+      alert ('Vous avez déjà atteint la quantité maximale autorisée pour ce produit');
+
+    } else if (findProduct.quantity < 100) {
+        findProduct.quantity += product.quantity;
+
+        if (findProduct.quantity > 100) {
+          alert ('Vous avez dépassé la quantité maximale autorisée de ce produit dans votre panier, la nouvelle quantité est de 100');
+          findProduct.quantity = 100;
+          
+        } else {
+            alert ('La quantité du produit a été mise à jour avec succès !');
+          }
+      }
+    
+    // Sinon le resultat est "undefined" et donc le produit n'existe pas dans le panier donc on le rajoute au tableau "basket".
+  } else {
+    basket.push(product);
+    alert ('Le produit a bien été ajouté dans votre panier !');
+  }
+
+  saveBasket(basket);
+};
